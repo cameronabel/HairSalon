7e1a73358af92f50a538cc358889f872989ce0d8
Cameron Abel <cameronabel@gmail.com>
2023-03-08 23:02:45 -0800

    Appointment model

---
+++ HairSalon/Models/Appointment.cs
@@ -0,0 +1,14 @@
+namespace HairSalon.Models;
+
+public class Appointment
+{
+  public int AppointmentId { get; set; }
+  public Client Client { get; set; }
+  public int ClientId { get; set; }
+  public Stylist Stylist { get; set; }
+  public int StylistId { get; set; }
+  public Specialty Specialty { get; set; }
+  public int SpecialtyId { get; set; }
+  public string Notes { get; set; }
+  public DateTime DateTime { get; set; }
+}
