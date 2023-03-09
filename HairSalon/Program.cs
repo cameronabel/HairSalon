076215ca5404233f172b54444f5c7bdf293489ed
Cameron Abel <cameronabel@gmail.com>
2023-03-08 22:52:13 -0800

    boilerplate file structure and default HTML

---
+++ HairSalon/Program.cs
@@ -0,0 +1,18 @@
+using HairSalon.Models;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+  name: "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
