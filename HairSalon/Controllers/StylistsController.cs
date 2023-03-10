d516ff9469865ef0ef6926532bc2d8544518b67c
Cameron Abel <cameronabel@gmail.com>
2023-03-10 02:42:22 -0800

    Stylist details date tweaks

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -42,7 +42,23 @@ public class StylistsController : Controller
   {
     Stylist thisStylist = _db.Stylists
                             .Include(stylist => stylist.Appointments)
+                            .Include(stylist => stylist.Specialty)
                             .FirstOrDefault(stylist => stylist.StylistId == id);
     return View(thisStylist);
+  }
+
+  public ActionResult Delete(int id)
+  {
+    Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+    return View(thisStylist);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+    _db.Stylists.Remove(thisStylist);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -6,9 +6,9 @@ public class Stylist
 {
   public int StylistId { get; set; }
   public string Name { get; set; }
-  public DateOnly HireDate { get; set; }
-  public DateOnly TerminationDate { get; set; }
-  public DateOnly RehireDate { get; set; }
+  public DateTime HireDate { get; set; }
+  public DateTime TerminationDate { get; set; }
+  public DateTime RehireDate { get; set; }
   public string Status { get; set; }
   public Specialty Specialty { get; set; }
   public int SpecialtyId { get; set; }
